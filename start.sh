6a79805f83634cc2e69cb99220492262a7d66616
alex-best <1987lex@gmail.com>
2024-06-28 01:20:58 +0200

    добавлены файлы окружения и работа с ними

--- start.sh
+++ start.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# Загрузка переменных окружения из .env файла
+set -a
+source .env
+set +a
+
+
 # Запуск базы данных
 docker-compose -f goip_db.yml up -d
 sleep 5
 
 # Проверка существования базы данных и её создание при необходимости
-db_ans=$(mysql -h 10.0.15.4 -P 3307 -u root -p123456 -e "show databases;")
-check_exist=$(echo $db_ans | grep goip)
+db_ans=$(mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASSWORD -e "show databases;")
+check_exist=$(echo "$db_ans" | grep $DB_NAME)
 if [[ -z $check_exist ]]; then
-    echo "No database"
-    mysql -h 10.0.15.4 -P 3307 -u root -p123456 -e "CREATE DATABASE goip;"
-    mysql -h 10.0.15.4 -P 3307 -u root -p123456 goip < ./goip.sql
+  echo "No database"
+  mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASSWORD -e "CREATE DATABASE $DB_NAME;"
+  mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASSWORD $DB_NAME < ./goip.sql
 else
-    echo "Database exists: $check_exist"
+  echo "Database exists: $check_exist"
 fi
 
 sleep 2
